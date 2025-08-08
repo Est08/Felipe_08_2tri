4a0ca1f4a5fb5ddef4de6389d7bd1bec388d8099
Est08 <140622100+Est08@users.noreply.github.com>
2025-08-08 18:58:39 -0300

    bre

--- main.js
+++ main.js
@@ -52,18 +52,18 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-classificaSenha(alfabeto.length);
+    classificaSenha(alfabeto.length);
 }
-function classificaSenha(tamanhoAlfabeto){
-let entropia = tamanhoSenha*Math.log2(tamanhoAlfabeto);
-forcaSenha.classList.remove('fraca', 'media', 'forte');
-if (entropia > 57){
-forcaSenha.classList.add('forte');
-} else if (entropia > 35 && entropia < 57) {
-forcaSenha.classList.add('media');
-} else if (entropia <= 35) {
-forcaSenha.classList.add('fraca');
-}
-const valorEntropia = document.querySelector('.entropia');
-valorEntropia.textContent = 2**Math.floor(entropia)/(100e6)*60*60*24;
+function classificaSenha(tamanhoAlfabeto) {
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (entropia > 57) {
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35) {
+        forcaSenha.classList.add('fraca');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = 2 ** Math.floor(entropia) / (100e6) * 60 * 60 * 24;
 }
